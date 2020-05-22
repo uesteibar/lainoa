d094cffdc75466cab0b32f4cc108130385e65e49
uesteibar <uesteibar@gmail.com>
2020-05-22 18:47:46 +0200

    Add REPL

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Lainoa
 
 Programming language built following the interpreterbook.com
+
+## Running locally
+
+You'll need [golang](https://golang.org/) installed.
+
+Install it:
+
+```shell
+make install
+```
+
+If you don't want to install it, you can also build the binary to `bin/lainoa`:
+
+```shell
+make build
+```
+
+Run the REPL:
+
+```shell
+lainoa repl
+```
---
+++ cmd/lainoa/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/uesteibar/lainoa/pkg/repl"
+)
+
+func printHelp() {
+	fmt.Println(`
+The following commands are available:
+
+	repl	start the lainoa REPL (interactive console)
+	help	print this nice little help`)
+}
+
+func startRepl() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Lainoa programming language\n",
+		user.Username)
+	fmt.Println("\nGo ahead and type some stuff!")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("You need to tell me what to do!")
+		printHelp()
+		return
+	}
+	action := os.Args[1]
+
+	switch action {
+	case "repl":
+		startRepl()
+	case "help":
+		printHelp()
+	default:
+		fmt.Printf("Command %s not supported\n", action)
+		printHelp()
+	}
+}
